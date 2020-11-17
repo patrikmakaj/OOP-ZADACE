2ec53c41346e960bbe8a1eabaa71c114b8ca2a92
patrikmakaj <patrikmakaj01@gmail.com>
2020-11-17 20:05:29 +0100

    test

---
+++ DZ2/DZ2/Description.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DZ2
+{
+    public class Description
+    {
+        private int number;
+        private TimeSpan lenght;
+        private string name;
+
+        public int Number { get => number; set => number = value; }
+        public TimeSpan Lenght { get => lenght; set => lenght = value; }
+        public String Name { get => name; set => name = value; }
+
+        public Description(int number, TimeSpan lenght, string name)
+        {
+            this.number = number;
+            this.lenght = lenght;
+            this.name = name;
+        }
+        public Description() { }
+
+        public override String ToString()
+        {
+            return $"{number},{lenght},{name}";
+        }
+        
+    }
+}
---
+++ DZ2/DZ2/TvUtilities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DZ2
+{
+    public class TvUtilities
+    {
+        public static Episode Parse(string epizoda)
+        {
+            //Problemi s učitavanjem doubleova, pa sam potražio riješenje na stackoverflow. Točku je gledalo kao separator za tisućice umjesto kao decimalni točku.
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone(); customCulture.NumberFormat.NumberDecimalSeparator = "."; System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            Episode ep = new Episode();
+            Description desc = new Description();
+            string[] tokens = epizoda.Split(',');
+            ep.ViewerCount = (Convert.ToInt32(tokens[0]));
+            //tokens[1] = tokens[1].Replace('.', ',');
+            ep.RatingSum = Double.Parse(tokens[1]);
+            //tokens[2] = tokens[2].Replace('.', ',');
+            ep.LargestRating = (Double.Parse(tokens[2]));
+            desc.Number = Convert.ToInt32(tokens[3]);
+            desc.Lenght = TimeSpan.Parse(tokens[4]);
+            desc.Name = tokens[5];
+            ep.Description = desc;
+            return ep;
+        }
+
+        public static void Sort(Episode[] episodes)
+        {
+            Episode[] sorted = new Episode[10];
+            int i, j;
+            for (i = 0; i < episodes.Length - 1; i++)
+            {
+                for (j = 0; j < episodes.Length - i - 1; j++)
+                {
+                    if (episodes[j] < episodes[j + 1])
+                    {
+                        Episode temp = new Episode();
+                        temp = episodes[j];
+                        episodes[j] = episodes[j + 1];
+                        episodes[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+}
